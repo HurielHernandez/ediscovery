77de39843e9bb1a22107998bc7824faf3d97db9a
Huriel Hernandez <huriel@Huriels-MacBook-Pro.local>
2016-12-22 15:34:58 -0600

    Create Initial Setup

---
+++ database/migrations/2016_12_22_211831_create_cases_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cases', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('case_number');
+            $table->integer('created_by')->unsigned();
+            $table->timestamps();
+            $table->foreign('created_by')->references('id')->on('users');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cases');
+    }
+}
